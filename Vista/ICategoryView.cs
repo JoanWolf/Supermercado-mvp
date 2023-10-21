ee13f21702ba73424782bc64dea412907de82ecd
Sebastian Velasco <dasstian@hotmail.com>
2023-10-21 14:59:22 -0500

    CategoryView

--- Vista/ICategoryView.cs
+++ Vista/ICategoryView.cs
@@ -8,23 +8,24 @@ namespace Supermarket_mvp.Vista
 {
     internal class ICategoryView
     {
-        string ModelId { get; set; }
-        string CategoryModelName { get; set; }
-        string CategoryModelObservation { get; set; }
-
-        string SearchValue { get; set; }
-        bool IsEdit { get; set; }
-        bool IsSuccesful { get; set; }
-        string Message { get; set; }
-
-        event EventHandler SearchEvent;
-        event EventHandler AddNewEvent;
-        event EventHandler EditEvent;
-        event EventHandler DeleteEvent;
-        event EventHandler SaveEvent;
-        event EventHandler CancelEvent;
-
-        void SetCategoryModelListBildingSource(BindingSource payModeList);
-        void Show();
+        
     }
 }
+string ModelId { get; set; }
+string CategoryModelName { get; set; }
+string CategoryModelObservation { get; set; }
+
+string SearchValue { get; set; }
+bool IsEdit { get; set; }
+bool IsSuccesful { get; set; }
+string Message { get; set; }
+
+event EventHandler SearchEvent;
+event EventHandler AddNewEvent;
+event EventHandler EditEvent;
+event EventHandler DeleteEvent;
+event EventHandler SaveEvent;
+event EventHandler CancelEvent;
+
+void SetCategoryModelListBildingSource(BindingSource payModeList);
+void Show();
