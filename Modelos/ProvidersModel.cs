da572cba173033717d4bf0701b769ee0f360f8cb
Sebastian Velasco <dasstian@hotmail.com>
2023-10-21 18:11:35 -0500

    IProvidersView

--- Modelos/ProvidersModel.cs
+++ Modelos/ProvidersModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace Supermarket_mvp.Modelos
 {
     internal class ProvidersModel
     {
+        [DisplayName("providers Id")]
+        public int Id { get; set; }
+
+        [DisplayName("providers Name")]
+        [Required(ErrorMessage = "providers name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "providers name must be between 3 and 50 characters")]
+
+        public string Name { get; set; }
+
+        [DisplayName("Observation")]
+        [Required(ErrorMessage = "providers Observation is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "providers observation must be between 3 and 200 characters")]
+        public string Observation { get; set; }
     }
 }
---
+++ Vista/IProvidersView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Vista
+{
+    internal interface IProvidersView
+    {
+        string ProvidersId { get; set; }
+        string ProvidersName { get; set; }
+        string ProvidersObservation { get; set; }
+
+        string SearchValue { get; set; }
+        bool IsEdit { get; set; }
+        bool IsSuccesful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        void SetProvidersListBildingSource(BindingSource payModeList);
+        void Show();
+    }
+}
