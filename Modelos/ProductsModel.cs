cd3dba536622588c841421fbd2b8af9e8bca56db
Sebastian Velasco <dasstian@hotmail.com>
2023-10-21 23:19:47 -0500

    ProducstModel

---
+++ Modelos/ProductsModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Modelos
+{
+    internal class ProductsModel
+    {
+        [DisplayName("Products Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Products Name")]
+        [Required(ErrorMessage = "Products name is required")]
+        [StringLength(80, MinimumLength = 3, ErrorMessage = "Products name must be between 3 and 80 characters")]
+        public string Name { get; set; }
+
+
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "Products Observation is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Products observation must be between 3 and 200 characters")]
+        public string description { get; set; }
+
+        [DisplayName("Price")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [DisplayName("Stock")]
+        [Required(ErrorMessage = "Stock is required")]
+        public int Stock { get; set; }
+
+        [DisplayName("Category Id")]
+        [Required(ErrorMessage = "Category Id is required")]
+        public int Category_Id { get; set; }
+
+
+    }
+}
---
+++ Vista/IProductsView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Vista
+{
+    internal interface IProductsView
+    {
+        string ProductsId { get; set; }
+        string ProductsName { get; set; }
+        string ProductsDescription { get; set; }
+        double ProductsPrice { get; set; }
+        int ProductsStock { get; set; }
+        string Products_Category { get; set; }
+
+        string SearchValue { get; set; }
+        bool IsEdit { get; set; }
+        bool IsSuccesful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        void SetProductsListBildingSource(BindingSource productsList);
+        void Show();
+    }
+}
