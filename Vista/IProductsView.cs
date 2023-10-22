fa366c773dc123aa13b683abd5ddb38d0ee365ab
Sebastian Velasco <dasstian@hotmail.com>
2023-10-21 23:22:13 -0500

    IProductsView

--- Vista/IProductsView.cs
+++ Vista/IProductsView.cs
@@ -8,13 +8,7 @@ namespace Supermarket_mvp.Vista
 {
     internal interface IProductsView
     {
-        string ProductsId { get; set; }
-        string ProductsName { get; set; }
-        string ProductsDescription { get; set; }
-        double ProductsPrice { get; set; }
-        int ProductsStock { get; set; }
-        string Products_Category { get; set; }
-
+        
         string SearchValue { get; set; }
         bool IsEdit { get; set; }
         bool IsSuccesful { get; set; }
